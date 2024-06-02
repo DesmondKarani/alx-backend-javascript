2fbb86fd8eddf23a7d9eb016d6393cae08b8c165
Desmond <karanidesmond@proton.me>
2024-06-02 14:52:44 +0300

    thn

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -10,10 +10,22 @@ export default class Currency {
   }
 
   set code(newCode) {
+    if (typeof newCode !== 'string') {
+      throw new TypeError('code must be a string');
+    }
     this._code = newCode;
   }
 
-  // Implement getter and setter for _name similarly
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('name must be a string');
+    }
+    this._name = newName;
+  }
 
   displayFullCurrency() {
     return `${this._name} (${this._code})`;

fa5081d749196802de8cf2a468eaaa2c69b7f366
Desmond <karanidesmond@proton.me>
2024-06-02 15:07:41 +0300

    hh

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,7 +1,12 @@
 // Task 5: Building class
 export default class Building {
   constructor(sqft) {
-    if (typeof sqft !== 'number') throw TypeError('sqft must be a number');
+    if (new.target === Building) {
+      throw new Error('Cannot instantiate abstract class');
+    }
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
     this._sqft = sqft;
   }
 
@@ -10,6 +15,7 @@ export default class Building {
   }
 
   // This method should be overridden by subclasses
+  // eslint-disable-next-line class-methods-use-this
   evacuationWarningMessage() {
     throw new Error('Class extending Building must override evacuationWarningMessage');
   }

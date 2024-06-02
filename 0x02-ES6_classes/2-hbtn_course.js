80bb608e65a4f03e84d013e7acdf84fe047822ee
Desmond <karanidesmond@proton.me>
2024-06-02 12:57:25 +0300

    git

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,24 @@
+// Task 2: HolbertonCourse class
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') throw TypeError('Name must be a string');
+    if (typeof length !== 'number') throw TypeError('Length must be a number');
+    if (!Array.isArray(students) || students.some(s => typeof s !== 'string')) {
+      throw TypeError('Students must be an array of strings');
+    }
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') throw TypeError('Name must be a string');
+    this._name = newName;
+  }
+
+  // Implement getters and setters for _length and _students similarly
+}

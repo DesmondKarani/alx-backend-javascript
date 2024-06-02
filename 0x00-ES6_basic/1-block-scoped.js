197749e6c964934882370f8be9afa1377549217a
Desmond <karanidesmond@proton.me>
2024-06-02 16:13:22 +0300

    dd

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,9 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    const taskIfTrue = true;
+    const task2IfTrue = false;
+    console.log(taskIfTrue, task2IfTrue); // Use the variables to avoid lint errors
   }
 
   return [task, task2];

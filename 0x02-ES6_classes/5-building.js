4f372d04159dc303451c18773f6e5946a44b328a
Desmond <karanidesmond@proton.me>
2024-06-02 15:18:26 +0300

    hhh

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,20 +1,15 @@
-// Task 5: Building class
 export default class Building {
   constructor(sqft) {
-    if (typeof sqft !== 'number') {
-      throw new TypeError('sqft must be a number');
+    if (new.target === Building) {
+      throw new Error('Building is an abstract class and cannot be instantiated directly.');
     }
     this._sqft = sqft;
-    if (new.target === Building) {
-      throw new Error('Cannot instantiate abstract class');
-    }
   }
 
   get sqft() {
     return this._sqft;
   }
 
-  // Enforce implementing evacuationWarningMessage in subclasses
   evacuationWarningMessage() {
     throw new Error('Class extending Building must override evacuationWarningMessage');
   }

e3d6b792e661029af36f5f775ae5ff7f1cfceab3
Desmond <karanidesmond@proton.me>
2024-06-04 17:43:34 +0300

    Automated commit message

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,10 +1,11 @@
 // 0-get_list_students.js
 
 function getListStudents() {
-  const students = [
+  return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
   ];
-  return students;
 }
+
+export default getListStudents;

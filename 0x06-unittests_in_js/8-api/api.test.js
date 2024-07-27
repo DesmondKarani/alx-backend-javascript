4ba78c7e7206ec34f53662241e672faef77979b3
Desmond Karani <karanidesmond@proton.me>
2024-07-27 14:20:27 +0300

    code

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,28 +1,27 @@
 const request = require('request');
 const { expect } = require('chai');
-const app = require('./api');
 
 describe('Index page', () => {
-    const port = 7865;
-    const url = `http://localhost:${port}/`;
+  const API_URL = 'http://localhost:7865';
 
-    before((done) => {
-        app.listen(port, () => {
-            done();
-        });
+  it('Correct status code', (done) => {
+    request.get(API_URL, (error, response) => {
+      expect(response.statusCode).to.equal(200);
+      done();
     });
+  });
 
-    it('should return status code 200', (done) => {
-        request(url, (error, response, body) => {
-            expect(response.statusCode).to.equal(200);
-            done();
-        });
+  it('Correct result', (done) => {
+    request.get(API_URL, (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
     });
+  });
 
-    it('should return the correct message', (done) => {
-        request(url, (error, response, body) => {
-            expect(body).to.equal('Welcome to the payment system');
-            done();
-        });
+  it('Other: Correct content-type', (done) => {
+    request.get(API_URL, (error, response) => {
+      expect(response.headers['content-type']).to.include('text/html');
+      done();
     });
+  });
 });
